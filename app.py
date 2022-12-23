2f56b9d60d9d3c6fb3fabd9701182090f2aa335e
sum-it-up-9 <66559261+sum-it-up-9@users.noreply.github.com>
2022-12-23 14:34:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -20,8 +20,17 @@ def load_data():
   # cur = conn.cursor().execute(query)
    return cur.fetch_pandas_all()
 
+@st.experimental_memo(ttl=600)
+def run_query(query):
+    with conn.cursor() as cur:
+        cur.execute(query)
+        return cur.fetchall()
+
+rows = run_query("SELECT * from mytable;")
+
+# Print results.
+for row in rows:
+    st.write(f"{row[0]} has a :{row[1]}:")
 
 
-df = load_data()
-st.write(df)
 

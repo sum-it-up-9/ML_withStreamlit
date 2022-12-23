8b32de88525df28f16b41017e9c54e88fddfd2c9
sum-it-up-9 <66559261+sum-it-up-9@users.noreply.github.com>
2022-12-23 13:55:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,4 @@
 import streamlit as st
 st.write("HI")
 print("hello")
-# set_page_config needs to be the first Streamlit command in your script
-st.set_page_config(layout="wide")
-st.title("CPG Forecasting Models Monitoring")
+

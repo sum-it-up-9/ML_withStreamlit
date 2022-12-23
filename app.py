949f6a5c39f8f184b014256dd36209da3b6350d0
sum-it-up-9 <66559261+sum-it-up-9@users.noreply.github.com>
2022-12-23 14:11:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,27 @@
 import streamlit as st
+import snowflake.connector
 # set_page_config needs to be the first Streamlit command in your script
 st.set_page_config(layout="wide")
 st.title("CPG Forecasting Models Monitoring")
 
+# Initialize connection.
+# Uses st.experimental_singleton to only run once.
+@st.experimental_singleton
+def init_connection():
+    return snowflake.connector.connect(
+        **st.secrets["snowflake"], client_session_keep_alive=True
+    )
+
+conn = init_connection()
+
+def load_data():
+   #rows = run_query("SELECT * from mytable;")
+   cur = conn.cursor().execute("SELECT count(*) from customers;")
+   #cur = conn.cursor().execute(query)
+   return cur.fetch_pandas_all()
+
+
+
+df = load_data()
+st.write(df)
 
